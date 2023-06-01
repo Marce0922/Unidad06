1942fb39ff5f9e01c1e63acd02551d44b7b276e0
= <=>
2023-05-31 20:26:24 -0500

    clase JSON 01 JavaScript

---
+++ JSON/json02.js
@@ -0,0 +1,10 @@
+let miobjeto01 = {
+    minumero : 12,
+    mistring : "Hola Mundo JavaScript",
+    mibooleano : true,
+}
+const JsonObj = JSON.stringify(miobjeto01)
+
+console.log(miobjeto01)
+console.log(miobjeto01.minumero)
+console.log(JsonObj)
